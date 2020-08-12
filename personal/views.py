3b66ba131d0a7b2724fa1c24f37bbf476f6317b8
mijeannot <mjeanot@protonmail.com>
2020-08-11 23:02:59 -0400

    pb with pagination template

--- personal/views.py
+++ personal/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
-from personal.models import Question
+from operator import attrgetter
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-# Create your views here.
-def home(request):
+from blog.views import get_blog_queryset
+from blog.models import BlogPost
+
+BLOG_POSTS_PER_PAGE = 10
+
+
+def home_screen_view(request, *args, **kwargs):
+
     context = {}
-    context["questions"] = Question.objects.all()
+
+    # Search
+    query = ""
+    if request.GET:
+        query = request.GET.get("q", "")
+        context["query"] = str(query)
+
+    blog_posts = sorted(
+        get_blog_queryset(query), key=attrgetter("date_updated"), reverse=True
+    )
+
+    # Pagination
+    page = request.GET.get("page", 1)
+    blog_posts_paginator = Paginator(blog_posts, BLOG_POSTS_PER_PAGE)
+    try:
+        blog_posts = blog_posts_paginator.page(page)
+    except PageNotAnInteger:
+        blog_posts = blog_posts_paginator.page(BLOG_POSTS_PER_PAGE)
+    except EmptyPage:
+        blog_posts = blog_posts_paginator.page(blog_posts_paginator.num_pages)
+
+    context["blog_posts"] = blog_posts
+
     return render(request, "personal/home.html", context)
 
-
-def about(request):
-    return render(request, "personal/about.html", {"aboutpage": "about page"})
-
-
-def contact(request):
-    return render(request, "personal/contact.html", {"contactpage": "contact us"})
